98faea02d21e938f8027dd9b6e347db7b7976fe5
sreij <chrj23020@g.nihon-u.ac.jp>
2025-11-21 14:57:50 +0900

    editedをEditedに変更

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <Link to="/"><img src="/pictures/Osake.png" alt="ロゴ画像" width={100} /></Link>
         <h1>今夜のベストマッチ！</h1>
       </div>
-      <h2>edited by Team osake</h2>
+      <h2>Edited by Team osake</h2>
     </header>
   );
 }
