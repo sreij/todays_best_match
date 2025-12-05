09eaa87a3817d9d3f74620176d327c2e9528ca39
sreij <chrj23020@g.nihon-u.ac.jp>
2025-12-05 15:14:50 +0900

    ナビゲーションバーの追加

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 
-// Webサイト上部のデザインを組み込むためのコンポーネントです。「<Header />でアクセス」
+// Webサイト上部のデザインを組み込むためのコンポーネントです。
 export default function Header() {
   return (
-     <header className="header">
-      <div className="header-left">
-        <Link to="/"><img src="/pictures/Osake.png" alt="ロゴ画像" width={100} /></Link>
-        <h1>今夜のベストマッチ！</h1>
-      </div>
-      <h2>Edited by Team osake</h2>
-    </header>
+    <>
+      <header className="header">
+        <div className="header-left">
+          <Link to="/"><img src="/pictures/Osake.png" alt="ロゴ画像" width={100} /></Link>
+          <h1>今夜のベストマッチ！</h1>
+        </div>
+
+      
+      </header>
+      <nav className="header-nav" aria-label="Main navigation">
+        <Link to="/">Home</Link>
+        <Link to="/Osake">Osake</Link>
+        <Link to="/Otsumami">Otsumami</Link>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
   );
 }
