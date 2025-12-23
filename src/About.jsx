445d902739a407a1d97a49f7ca036d2a4b18346a
sreij <chrj23020@g.nihon-u.ac.jp>
2025-12-23 10:59:45 +0900

    Aboutページの、更新準備

--- src/About.jsx
+++ src/About.jsx
@@ -6,6 +6,11 @@ export default function About() {
       <section className="about">
         <h1>このサイトについて</h1>
         <p>「今夜のベストマッチ！」は、お酒とおつまみの新しいマッチを見つけるためのサイトです。</p>
+        <introduction>
+          <h2>紹介動画</h2>
+          <p>このサイトの紹介動画をご覧ください。</p>
+          !!!VIDEO_PLACEHOLDER!!!
+        </introduction>
         <author>
           <h2>このサイトの制作者</h2>
           <h3>Team Osake</h3>
@@ -15,6 +20,12 @@ export default function About() {
           5423020 齋藤 礼二
         </author>
         このコンテンツは、日本大学文理学部情報科学科デジタルコンテンツの制作課題です。
+        <br/>
+        <a href="https://github.com/sreij/todays_best_match">GitHub リポジトリ</a>
+        <br />
+        <a href="">企画書</a>(アップロードしてパスを追加)
+        <br />
+        <a href="">説明文書</a>(アップロードしてパスを追加)
       </section>
     </main>
   );
